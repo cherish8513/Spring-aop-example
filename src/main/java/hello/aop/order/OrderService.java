e37e7ef21e92fbeeee730365865b0de1a2741539
cherish8513 <cherish8513@naver.com>
2021-12-20 17:43:37 +0900

    Aop Example

---
+++ src/main/java/hello/aop/order/OrderService.java
@@ -0,0 +1,15 @@
+package hello.aop.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void orderItem(String orderItem) {
+        orderRepository.save(orderItem);
+    }
+}
